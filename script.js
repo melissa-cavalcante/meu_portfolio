95b1fcd2cac193719c3fdfa24ccf89c81aa2b16e
Melissa <melissa.massimino@gmail.com>
2025-07-08 23:34:37 -0300

    html semantico atualizado e corrigido no css

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ document.querySelectorAll(".project_unit").forEach(project => {
         document.getElementById("project_img").setAttribute("src", `./assets/img/img-preview.svg`);
     })
 })
+
+const links = document.querySelectorAll(".nav_list a");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        links.forEach(l => l.classList.remove('active_page'));
+
+        link.classList.add('active_page');
+    })
+})
