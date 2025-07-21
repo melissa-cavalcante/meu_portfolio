f26815071a21ea07ab2504f91ec061cb06492622
Melissa <melissa.massimino@gmail.com>
2025-07-20 22:20:13 -0300

    observador inserido na navbar

---
+++ script.js
@@ -0,0 +1,30 @@
+const sections = document.querySelectorAll('#home, #sobre, #projetos, #contato');
+const navLinks = document.querySelectorAll('nav ul li a'); // links do menu
+
+const options = {
+  root: null,
+  threshold: 0.10 // quando 60% da seção está visível
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // Remove a classe ativa de todos os links
+      navLinks.forEach(link => {
+        link.classList.remove('change-color');
+      });
+
+      // Adiciona a classe ativa ao link correspondente à seção visível
+      const id = entry.target.getAttribute('id'); // pega o id da seção
+      const activeLink = document.querySelector(`nav a[href="#${id}"]`);
+      if (activeLink) {
+        activeLink.classList.add('change-color');
+      }
+    }
+  });
+}, options);
+
+// Observar todas as seções
+sections.forEach(section => {
+  observer.observe(section);
+});
